d97298564f03684dc730aa4329733029fa8058f0
maksymmarych <maksymmarych@gmail.com>
2018-02-27 16:34:59 +0200

    #add list of recipes with image

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,17 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Recipe } from './recipe/recipe.module';
-
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe('Test Recipe','Test','https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg')
-  ];
-
   constructor() { }
 
   ngOnInit() {
